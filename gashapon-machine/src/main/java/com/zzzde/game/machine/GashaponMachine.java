686b422a814532f9cd72b6677d63b2e53bd0d169
zhuhx <zhuhangxuan@yugu.net.cn>
2023-03-21 11:31:20 +0800

    新建ProjectT

---
+++ gashapon-machine/src/main/java/com/zzzde/game/machine/GashaponMachine.java
@@ -0,0 +1,18 @@
+package com.zzzde.game.machine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zzzde
+ * @version 1.0
+ * @date 2023/3/21 11:26
+ */
+@SpringBootApplication
+public class GashaponMachine {
+    public static void main(String[] args) {
+        SpringApplication.run(GashaponMachine.class, args);
+    }
+}
+
+
---
+++ numerical-designer/src/main/java/com/zzzde/game/numerical/designer/NumericalDesignerApplication.java
@@ -0,0 +1,13 @@
+package com.zzzde.game.numerical.designer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumericalDesignerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumericalDesignerApplication.class, args);
+    }
+
+}
